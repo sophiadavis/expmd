271416efc88c2c16b1f01211dde041c796ba6ec1
José Valim <jose.valim@plataformatec.com.br>
2014-08-21 13:12:11 -0400

    Receive and send alive requests/responses

--- lib/expmd.ex
+++ lib/expmd.ex
@@ -19,8 +19,8 @@ defmodule Expmd do
 
   def accept(port) do
     {:ok, socket} = :gen_tcp.listen(port,
-                      [:binary, packet: :line, active: false, reuseaddr: true])
-    IO.puts "Accepting connections on port #{port}"
+                      [:binary, packet: 0, active: false, reuseaddr: true])
+    Logger.info "Accepting connections on port #{port}"
     loop_acceptor(socket)
   end
 
@@ -30,21 +30,28 @@ defmodule Expmd do
     loop_acceptor(socket)
   end
 
+  @alive_req 120
+  @alive_resp 121
+
   defp serve(socket) do
-    case read_line(socket) do
-      {:ok, msg} ->
-        write_line(socket, msg)
+    case next_message(socket) do
+      {:ok, <<@alive_req, port::16, node_type, protocol, high_version::16, low_version::16,
+              nlen::16, node::binary-size(nlen), elen::16, extra::binary-size(elen)>> = msg} ->
+        Logger.debug "Alive request: #{node} - #{port}"
+        :gen_tcp.send(socket, <<@alive_resp, 0, 1::16>>)
         serve(socket)
-      {:error, reason} -> 
+      {:error, reason} ->
         Logger.info "TCP exited with reason: #{inspect reason}"
     end
   end
 
-  defp read_line(socket) do
-    :gen_tcp.recv(socket, 0)
-  end
-
-  defp write_line(socket, msg) do
-    :gen_tcp.send(socket, msg)
+  defp next_message(socket) do
+    case :gen_tcp.recv(socket, 2) do
+      {:ok, <<int::16>>} ->
+        Logger.debug "Reading message with length: #{int}"
+        :gen_tcp.recv(socket, int)
+      {:error, _} = error ->
+        error
+    end
   end
 end
